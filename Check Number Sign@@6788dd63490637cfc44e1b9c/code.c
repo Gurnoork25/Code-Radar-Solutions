eb637b1f7360a8c4fa4ee36d71317e5c19bd5782
Gurnoork25 <gurnoor0304.becse24@chitkara.edu.in>
2025-03-20 00:29:35 +0530

    Solution to Check Number Sign

--- Check Number Sign@@6788dd63490637cfc44e1b9c/code.c
+++ Check Number Sign@@6788dd63490637cfc44e1b9c/code.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-
 int main() {
     int a;
     scanf("%d",&a);
-    if(a%2==0){
-        printf("Even");
+    if(a>0){
+        printf("Positive");
+    }
+    else if(a==0){
+        printf("Zero");
     }
     else{
-        printf("Odd");
+        printf("Negative");
     }
     return 0;
 }
