b3bb7bfebd87421434f70f9a7180869903f4f380
Gurnoork25 <gurnoor0304.becse24@chitkara.edu.in>
2025-03-20 00:03:00 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -13,10 +13,10 @@ int main() {
         printf("C");
     }
     else if(a>=60 && a>70){
-        printf("D")
+        printf("D");
     }
     else{
-        printd("F");
+        printf("F");
     }
     return 0;
 }        
